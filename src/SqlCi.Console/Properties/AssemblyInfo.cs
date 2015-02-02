c7bf8ff5354bdaa56b5c58becd70d3c4b11a4291
Wes Shaddix <wes.shaddix@outlook.com>
2015-02-02 00:21:44 -0500

    working towards json based config

--- src/SqlCi.Console/Properties/AssemblyInfo.cs
+++ src/SqlCi.Console/Properties/AssemblyInfo.cs
@@ -20,10 +20,10 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1b5d1aca-9c96-4baa-b71b-ad4f2ec579eb")]
 
 // Version information for an assembly consists of the following four values:
-//
+// 
 // Major Version Minor Version Build Number Revision
-//
+// 
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
